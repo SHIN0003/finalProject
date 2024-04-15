c712af92bcb654e927d5905acd46420dcb070269
Andrew Tai <andrewtai@umass.edu>
2024-04-15 17:10:54 -0400

    planner frontend functionality

--- src/client/script.js
+++ src/client/script.js
@@ -1,20 +1,39 @@
 // Workout Scheduler: Users could interactively create and modify their workout schedules. 
-let workoutSchedule = JSON.parse(localStorage.getItem('workoutSchedule')) || {};
-function addWorkout(date, workout) {
-    if (date && workout) {
-        workoutSchedule[date] = workout;
-        localStorage.setItem('workoutSchedule', JSON.stringify(workoutSchedule));
-        document.getElementById('schedule').innerHTML += `<p>${date}: ${workout}</p>`;
-        document.getElementById('days').value = '';
+let weeklySchedule = JSON.parse(localStorage.getItem('weeklySchedule')) || {};
+
+function submitSchedule(workoutSchedule) {
+    if (workoutSchedule) {
+        //first index is useless
+        let schedule = workoutSchedule.split('<p>');
+        localStorage.setItem('weeklySchedule', JSON.stringify(schedule));
+        document.getElementById('schedule').innerHTML = '';
+        alert('Schedule submitted successfully.')
+    } else {
+        alert('Please enter a valid schedule.');
+    }
+}
+
+document.getElementById('submit-button').addEventListener('click', function() {
+    submitSchedule(document.getElementById('schedule').innerHTML);
+});
+
+function addWorkout(day, workout) {
+    if (day && workout) {
+        document.getElementById('schedule').innerHTML += `<p>${day}: ${workout}</p>`;
+        document.getElementById('day').value = '';
         document.getElementById('workout-input').value = '';
     } else {
-        alert('Please enter a valid date and workout.');
+        alert('Please enter a valid day and workout.');
     }
 }
 document.getElementById('add-button').addEventListener('click', function() {
-    let date = document.getElementById('days').value;
+    let day = document.getElementById('day').value;
     let workout = document.getElementById('workout-input').value;
-    addWorkout(date, workout);
+    addWorkout(day, workout);
+});
+
+document.getElementById('clear-button').addEventListener('click', function() {
+    document.getElementById('schedule').innerHTML = '';
 });
 
 // Goal Tracker: Allows users to set goals and track their progress.
