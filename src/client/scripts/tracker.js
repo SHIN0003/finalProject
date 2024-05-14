76d53cd308ca761ec1a44e83ccf45861b0a79e46
Tejas Patel <122117679+tpatel09@users.noreply.github.com>
2024-05-14 14:29:38 -0400

    Revert "small fix"
    This reverts commit 825cf8d17797c97d3b602e8845f5fdb036a6858c.

--- src/client/scripts/tracker.js
+++ src/client/scripts/tracker.js
@@ -95,7 +95,7 @@ function handleSaveHabit() {
         .catch(error => {
             console.error('Error saving habit:', error);
         });
-
+}
 
 // Function to increment the streaks number
 function incrementStreak() {
